35018096727c7e13a144c708b4835e29bd5b9a9d
bhaskarm <imbhaskaran@gmail.com>
2022-11-18 19:36:28 +0530

    editor working

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -1,22 +1,45 @@
-import { useState } from 'react';
-import { EditorState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { useState } from "react";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import { DOMPurify } from "dompurify";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const MyEditor = () => {
-    const [editorState, setEditorState] = useState(
-        () => EditorState.createEmpty(),
-      );
-      console.log(editorState)
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  };
+
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  };
+
+  const createMarkup = (html) => {
+    return console.log(html)
+  
+  };
+  console.log(createMarkup(convertedContent))
   return (
-    <Editor editorState={editorState}  
-      onEditorStateChange={setEditorState}
-      wrapperClassName="wrapper-class"
-      editorClassName="editor-class"
+    <>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={handleEditorChange}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbar={{
+            options: ['inline', 'blockType', 'list', 'textAlign', 'image'],
+        }}
       />
-  )
-}
-
-
-
-export default MyEditor
+      
+    </>
+  );
+};
+export default MyEditor;
